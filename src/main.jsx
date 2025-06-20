7fbca565fe60dbb04a2f1ff659a8ef5c7ecc867c
msanjai2005 <msanjai1012@gmail.com>
2025-06-20 23:09:22 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Navbar from './Components/Navbar.jsx'
-import Counter from './Components/Counter.jsx'
-import Todo from './Components/Todo.jsx'
-import NotFound from './Components/NotFound.jsx'
-import Weather from './Weather.jsx'
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Weather/>,
-    errorElement:<NotFound/>
-  },
-  {
-    path:'/counter',
-    element:<Counter/>
-  },
-  {
-    path:'/app',
-    element:<App/>
-  },
-  {
-    path:'/to-do-list',
-    element:<Navbar/>
-  }
-])
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Weather/>,
+//     errorElement:<NotFound/>
+//   },
+//   {
+//     path:'/counter',
+//     element:<Counter/>
+//   },
+//   {
+//     path:'/app',
+//     element:<App/>
+//   },
+//   {
+//     path:'/to-do-list',
+//     element:<Navbar/>
+//   }
+// ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </StrictMode>
 )
